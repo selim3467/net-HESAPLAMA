b1f71dc9988faf1bfe378475abc98d41db16bd12
PC <PC@DESKTOP-TKG6CEA>
2023-05-23 17:49:13 +0300

    Add project files.

---
+++ net HESAPLAMA/Form1.Designer.cs
@@ -0,0 +1,320 @@
+﻿namespace net_HESAPLAMA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtmatematikdoru = new System.Windows.Forms.TextBox();
+            this.txtmatematikyanlıs = new System.Windows.Forms.TextBox();
+            this.txtmatnet = new System.Windows.Forms.TextBox();
+            this.txtturkcedoru = new System.Windows.Forms.TextBox();
+            this.txtturkcenet = new System.Windows.Forms.TextBox();
+            this.txtkımyadoru = new System.Windows.Forms.TextBox();
+            this.txtkımyanet = new System.Windows.Forms.TextBox();
+            this.txtfızıkdoru = new System.Windows.Forms.TextBox();
+            this.txtfızıknet = new System.Windows.Forms.TextBox();
+            this.txtturkceyanlıs = new System.Windows.Forms.TextBox();
+            this.txtkımyayanlıs = new System.Windows.Forms.TextBox();
+            this.txtfızıkyanlıs = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtsonuc = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.Blue;
+            this.label1.Location = new System.Drawing.Point(204, 86);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(123, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "MATEMATİK";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(204, 141);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 22);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "TÜRKÇE";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(204, 208);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(69, 22);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "KİMYA";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Blue;
+            this.label4.Location = new System.Drawing.Point(204, 276);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(57, 22);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "FİZİK";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.ForeColor = System.Drawing.Color.Lime;
+            this.label5.Location = new System.Drawing.Point(344, 29);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(24, 22);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "D";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.ForeColor = System.Drawing.Color.Red;
+            this.label6.Location = new System.Drawing.Point(457, 29);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(23, 22);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Y";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.ForeColor = System.Drawing.Color.Blue;
+            this.label7.Location = new System.Drawing.Point(532, 29);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(50, 22);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "NET";
+            // 
+            // txtmatematikdoru
+            // 
+            this.txtmatematikdoru.Location = new System.Drawing.Point(334, 75);
+            this.txtmatematikdoru.Multiline = true;
+            this.txtmatematikdoru.Name = "txtmatematikdoru";
+            this.txtmatematikdoru.Size = new System.Drawing.Size(64, 33);
+            this.txtmatematikdoru.TabIndex = 1;
+            // 
+            // txtmatematikyanlıs
+            // 
+            this.txtmatematikyanlıs.Location = new System.Drawing.Point(438, 75);
+            this.txtmatematikyanlıs.Multiline = true;
+            this.txtmatematikyanlıs.Name = "txtmatematikyanlıs";
+            this.txtmatematikyanlıs.Size = new System.Drawing.Size(64, 33);
+            this.txtmatematikyanlıs.TabIndex = 1;
+            // 
+            // txtmatnet
+            // 
+            this.txtmatnet.Location = new System.Drawing.Point(532, 75);
+            this.txtmatnet.Multiline = true;
+            this.txtmatnet.Name = "txtmatnet";
+            this.txtmatnet.Size = new System.Drawing.Size(64, 33);
+            this.txtmatnet.TabIndex = 1;
+            // 
+            // txtturkcedoru
+            // 
+            this.txtturkcedoru.Location = new System.Drawing.Point(334, 127);
+            this.txtturkcedoru.Multiline = true;
+            this.txtturkcedoru.Name = "txtturkcedoru";
+            this.txtturkcedoru.Size = new System.Drawing.Size(64, 33);
+            this.txtturkcedoru.TabIndex = 1;
+            // 
+            // txtturkcenet
+            // 
+            this.txtturkcenet.Location = new System.Drawing.Point(532, 127);
+            this.txtturkcenet.Multiline = true;
+            this.txtturkcenet.Name = "txtturkcenet";
+            this.txtturkcenet.Size = new System.Drawing.Size(64, 33);
+            this.txtturkcenet.TabIndex = 1;
+            // 
+            // txtkımyadoru
+            // 
+            this.txtkımyadoru.Location = new System.Drawing.Point(334, 197);
+            this.txtkımyadoru.Multiline = true;
+            this.txtkımyadoru.Name = "txtkımyadoru";
+            this.txtkımyadoru.Size = new System.Drawing.Size(64, 33);
+            this.txtkımyadoru.TabIndex = 1;
+            // 
+            // txtkımyanet
+            // 
+            this.txtkımyanet.Location = new System.Drawing.Point(532, 197);
+            this.txtkımyanet.Multiline = true;
+            this.txtkımyanet.Name = "txtkımyanet";
+            this.txtkımyanet.Size = new System.Drawing.Size(64, 33);
+            this.txtkımyanet.TabIndex = 1;
+            // 
+            // txtfızıkdoru
+            // 
+            this.txtfızıkdoru.Location = new System.Drawing.Point(334, 265);
+            this.txtfızıkdoru.Multiline = true;
+            this.txtfızıkdoru.Name = "txtfızıkdoru";
+            this.txtfızıkdoru.Size = new System.Drawing.Size(64, 33);
+            this.txtfızıkdoru.TabIndex = 1;
+            // 
+            // txtfızıknet
+            // 
+            this.txtfızıknet.Location = new System.Drawing.Point(532, 265);
+            this.txtfızıknet.Multiline = true;
+            this.txtfızıknet.Name = "txtfızıknet";
+            this.txtfızıknet.Size = new System.Drawing.Size(64, 33);
+            this.txtfızıknet.TabIndex = 1;
+            // 
+            // txtturkceyanlıs
+            // 
+            this.txtturkceyanlıs.Location = new System.Drawing.Point(438, 130);
+            this.txtturkceyanlıs.Multiline = true;
+            this.txtturkceyanlıs.Name = "txtturkceyanlıs";
+            this.txtturkceyanlıs.Size = new System.Drawing.Size(64, 33);
+            this.txtturkceyanlıs.TabIndex = 1;
+            // 
+            // txtkımyayanlıs
+            // 
+            this.txtkımyayanlıs.Location = new System.Drawing.Point(438, 200);
+            this.txtkımyayanlıs.Multiline = true;
+            this.txtkımyayanlıs.Name = "txtkımyayanlıs";
+            this.txtkımyayanlıs.Size = new System.Drawing.Size(64, 33);
+            this.txtkımyayanlıs.TabIndex = 1;
+            // 
+            // txtfızıkyanlıs
+            // 
+            this.txtfızıkyanlıs.Location = new System.Drawing.Point(438, 265);
+            this.txtfızıkyanlıs.Multiline = true;
+            this.txtfızıkyanlıs.Name = "txtfızıkyanlıs";
+            this.txtfızıkyanlıs.Size = new System.Drawing.Size(64, 33);
+            this.txtfızıkyanlıs.TabIndex = 1;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.ForeColor = System.Drawing.Color.Blue;
+            this.label8.Location = new System.Drawing.Point(732, 86);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(136, 22);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "TOPLAM NET";
+            // 
+            // txtsonuc
+            // 
+            this.txtsonuc.Location = new System.Drawing.Point(736, 130);
+            this.txtsonuc.Multiline = true;
+            this.txtsonuc.Name = "txtsonuc";
+            this.txtsonuc.Size = new System.Drawing.Size(132, 57);
+            this.txtsonuc.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.Location = new System.Drawing.Point(334, 339);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 58);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "HESAPLA";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 22F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1100, 619);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtfızıknet);
+            this.Controls.Add(this.txtsonuc);
+            this.Controls.Add(this.txtkımyanet);
+            this.Controls.Add(this.txtturkcenet);
+            this.Controls.Add(this.txtmatnet);
+            this.Controls.Add(this.txtfızıkdoru);
+            this.Controls.Add(this.txtkımyadoru);
+            this.Controls.Add(this.txtturkcedoru);
+            this.Controls.Add(this.txtfızıkyanlıs);
+            this.Controls.Add(this.txtkımyayanlıs);
+            this.Controls.Add(this.txtturkceyanlıs);
+            this.Controls.Add(this.txtmatematikyanlıs);
+            this.Controls.Add(this.txtmatematikdoru);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtmatematikdoru;
+        private System.Windows.Forms.TextBox txtmatematikyanlıs;
+        private System.Windows.Forms.TextBox txtmatnet;
+        private System.Windows.Forms.TextBox txtturkcedoru;
+        private System.Windows.Forms.TextBox txtturkcenet;
+        private System.Windows.Forms.TextBox txtkımyadoru;
+        private System.Windows.Forms.TextBox txtkımyanet;
+        private System.Windows.Forms.TextBox txtfızıkdoru;
+        private System.Windows.Forms.TextBox txtfızıknet;
+        private System.Windows.Forms.TextBox txtturkceyanlıs;
+        private System.Windows.Forms.TextBox txtkımyayanlıs;
+        private System.Windows.Forms.TextBox txtfızıkyanlıs;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtsonuc;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ net HESAPLAMA/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace net_HESAPLAMA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double matdoru = Convert.ToDouble(txtmatematikdoru.Text);
+            double matyanlis = Convert.ToDouble(txtmatematikyanlıs.Text);
+
+            double turkcedoru = Convert.ToDouble(txtturkcedoru.Text);
+            double turkceyanlis = Convert.ToDouble(txtturkceyanlıs.Text);
+
+            double kimyadoru = Convert.ToDouble(txtkımyadoru.Text);
+            double kimyayanlis = Convert.ToDouble(txtkımyayanlıs.Text);
+
+            double fizikdoru = Convert.ToDouble(txtfızıkdoru.Text);
+            double fizikyanlis = Convert.ToDouble(txtfızıkyanlıs.Text);
+
+            double matnet = matdoru - (matyanlis / 4);
+            double turknet = turkcedoru - (turkceyanlis / 4);
+            double kimyanet = kimyadoru - (kimyayanlis / 4);
+            double fiziknet = kimyadoru - (kimyayanlis / 4);
+
+
+            txtmatnet.Text = matnet.ToString();
+            txtturkcenet.Text = turknet.ToString();
+            txtkımyanet.Text = kimyanet.ToString();
+            txtfızıknet.Text = fiziknet.ToString();
+
+            double net = Convert.ToDouble(matnet + turknet + kimyanet + fiziknet);
+            txtsonuc.Text = net.ToString();
+
+        }
+    }
+}
